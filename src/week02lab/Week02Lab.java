fd5a55b74adeb11336e565d34e65cf506ba29e25
Vladik <Vladik@DESKTOP-UKIL8PR>
2022-09-18 11:33:42 -0600

    Add Print Added print of hello world

--- src/week02lab/Week02Lab.java
+++ src/week02lab/Week02Lab.java
@@ -16,6 +16,7 @@ public class Week02Lab {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
